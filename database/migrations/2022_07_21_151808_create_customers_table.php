fb9fa90262ca923bab69cf1e3e738b7e83f3b82f
pratik_index <herecomeslucky0145@gmail.com>
2022-07-21 23:28:16 +0545

    starting

---
+++ database/migrations/2022_07_21_151808_create_customers_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('full_name');
+            $table->string('designation');
+            $table->string('company_type_id');
+            $table->string('company_name')->unique();
+            $table->text('company_description');
+            $table->string('company_phone')->unique();
+            $table->string('company_email')->unique();
+            $table->string('company_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
---
+++ database/migrations/2022_07_21_151832_create_members_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('full_name');
+            $table->string('address');
+            $table->boolean('is_recruited')->default(false);
+            $table->integer('recruited_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+};
